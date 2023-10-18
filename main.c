b066ee2fc7d1a58f8871b399d07ad1d4a2c5dabf
0x41mujt4b4 <almujtaba007@gmail.com>
2023-10-18 14:25:49 -0700

    fix betty error

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - function that checks if our shell is called
+ * main - The main function
  *
  * Return: 0 on success
  */
 int main(void)
 {
-	/* check interactive or none interactive mode */
 	if (isatty(STDIN_FILENO) == 1)
 	{
 		char *line;
@@ -19,14 +18,10 @@ int main(void)
 			line = read_line(); /* read line from stdin */
 			args = split_line(line); /* tokenize line */
 			status = execute_args(args);
-			/* avoid memory leaks */
 			free(line);
 			free(args);
-			/* exit with status */
 			if (status >= 0)
-			{
 				exit(status);
-			}
 		} while (status == -1);
 	}
 	else
@@ -39,14 +34,10 @@ int main(void)
 			line = read_stream();
 			args = split_line(line); /* tokenize line */
 			status = execute_args(args);
-			/* avoid memory leaks */
 			free(line);
 			free(args);
-			/* exit with status */
 			if (status >= 0)
-			{
 				exit(status);
-			}
 		} while (status == -1);
 	}
 	return (0);
